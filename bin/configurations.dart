5b53a7bfdb0621c9525b8ae7b9f1375d2d18c349
hhumbetovv <100850799+hhumbetovv@users.noreply.github.com>
2023-03-08 13:24:48 +0400

    init

---
+++ bin/configurations.dart
@@ -0,0 +1,14 @@
+class Configurations {
+  //! Realtime Database Url in Firebase
+  static const databaseUrl = "<Database Url>";
+
+  //! Web app's Firebase configuration
+  static const firebaseConfig = {
+    "apiKey": "<apiKey>",
+    "authDomain": "<authDomain>",
+    "projectId": "<projectId>",
+    "storageBucket": "<storageBucket>",
+    "messagingSenderId": "<messagingSenderId>",
+    "appId": "<appId>"
+  };
+}
